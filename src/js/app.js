4c22da2b039f7dc1aa61413d1ffe2f079719f4bc
sole <sole@mozilla.com>
2015-04-11 22:54:02 +0100

    it... WORKS! O_O

--- src/js/app.js
+++ src/js/app.js
@@ -3,15 +3,40 @@ window.addEventListener('load', function() {
   'use strict';
 
   var getNetworkInfo = require('get-network-info');
-  var HttpServer = require('fxos-web-server');
-  var divInfo = document.getElementById('info'); 
+  var HTTPServer = require('fxos-web-server');
+  var divInfo = document.getElementById('info');
+  var divMessages = document.getElementById('messages');
 
   // network info
   var info = getNetworkInfo();
   if(info) {
-	  divInfo.innerHTML = `<strong>${info.ip}</strong>`;
+    divInfo.innerHTML = `<strong>${info.ip}</strong>`;
   }
 
-  // requests below
+  // setting up server
+  var server = new HTTPServer(80);
+
+  server.addEventListener('request', function(evt) {
+    var request = evt.request;
+    var response = evt.response;
+
+    var result = `requested: ${request.path}, thank you`;
+
+    log('requested? ' + request.path);
+
+    response.send(result);
+  });
+
+  server.start(); // TODO is this sync? do we get errors returned?
+
+  log('server up');
+
+  window.addEventListener('beforeunload', function() {
+    server.stop();
+  });
+
+  function log(message) {
+    divMessages.innerHTML += message + '<br />';
+  }
 
 });
